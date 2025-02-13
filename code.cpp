d2ce46e6339e0e9f2c694a5f45b3522097f4b9c3
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-02-14 03:10:26 +0530

    started cpp

--- code.cpp
+++ code.cpp
@@ -1,18 +1,79 @@
+//libraries
+#include <iostream> //input output stream library
+#include <bits/stdc++.h> //standard c++ library
 
-#include <iostream>
-using namespace std;
+//namespaces
+using namespace std; //it helps not write std:: in standard fucntions
+//it tells computer,use all names of std namespace without explicit state std:: each time
+//with std (standard namespace)
+//cout<<"hi";
+//else
+//std::cout<<"hi";
 
-int main() {
-    int n = 0;
-
-    do {
-        cout<<"yay";
-    }while (n>0);
+//single line comment
+/*
+Multiline comment
+*/
 
 
 
-    return 0;
+//main code start
+int main(){
+    //Code Starts
+    //indentation don't matter
+
+    // cout function (from iostream library)
+    // std::cout<<"hi";
+    cout<<"hi";
+    cout<<endl<<"kintsugi";
+
+    //new line
+    cout<< "\n";     //way1 (faster)
+    cout<< endl ;    //way2 
+    
+    int x;
+    //cin stream, take user input 
+    cin>>x;
+
+    int y;
+    //taking multiple inputs
+    cin>>x>>y;
+    /*
+    by     cin>>x>>y;
+    we can use any of them for input ways:
+    input way 1:
+    10 20
+
+    input way 2:
+    10
+    20
+
+    both kind of inputs are valid :)
+    */
+
+
+   // Datatypes (inbuilt)
+   // Declare ways
+   // datatype var_name,var_name2,etc;
+   // datatype var_name= value;
+
+   int x1;      //int 4bytes
+   long x2;     //long (unsigned short 2bytes, long 4bytes, unsigned long 4bytes)
+   long long x3; //long long 8bytes
+   float x4;    //float 4bytes
+   double x5;   //double 8bytes
+   // they all have simple cin,cout
+
+   string x6,x7,x8;// String
+   cin>> x6 >> x7; //input till space "hi bali"
+   getline(cin,x8); //input string till linebreak 
+   
+
+
+ 
+
+
+
+    
+    return 0;//End of code
 }
-
-
-// Output: yay
